de08cc8a3b64af9c7ed1bee629d7f073d90312a2
Luiz Paulo dev <luizpaulo.dev.apps@gmail.com>
2025-11-22 04:06:40 +0000

    precisamos prepar o projeto pra deploy cire um .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6338519632-51247",
-  "appId": "1:974521544060:web:a6c50796ce44ea12a788f9",
-  "apiKey": "AIzaSyDirPCmPvyVSPGRnZ4BjMNx57fcqj0Zfs4",
-  "authDomain": "studio-6338519632-51247.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "974521544060"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
