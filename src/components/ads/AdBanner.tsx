f26df3ba05384ab938c3a9156b52f14134da83ca
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2025-11-22 20:58:33 -0300

    Add

--- src/components/ads/AdBanner.tsx
+++ src/components/ads/AdBanner.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React, { useEffect } from 'react';
-import Script from 'next/script';
 import { usePathname } from 'next/navigation';
 
 declare global {
   interface Window {
-    adsbygoogle: { [key: string]: unknown }[];
+    adsbygoogle?: { [key: string]: unknown }[];
   }
 }
 
@@ -15,29 +14,23 @@ const AdBanner = () => {
 
   useEffect(() => {
     try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      if (typeof window !== "undefined") {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
     } catch (err) {
       console.error(err);
     }
   }, [pathname]);
 
   return (
-    <>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5750464088623363"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-5750464088623363"
-        data-ad-slot="5124339999"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-5750464088623363"
+      data-ad-slot="3856507618"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   );
 };
 
