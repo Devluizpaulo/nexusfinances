214e8c2b677a09541f6be5039f4f590084b4d3c7
Luiz Paulo dev <luizpaulo.dev.apps@gmail.com>
2025-12-13 23:40:56 +0000

    quero mais melhoruiar

--- src/app/expenses/health/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthenticatedLayout } from "@/components/layout/authenticated-layout";
-
-export default function SalaryLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
